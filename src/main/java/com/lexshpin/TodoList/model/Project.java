04d14351cee407e320856d93847328dec6d06436
lexshpin <lexshpin@gmail.com>
2022-12-11 16:38:38 -0500

    Model for project done

--- src/main/java/com/lexshpin/TodoList/model/Project.java
+++ src/main/java/com/lexshpin/TodoList/model/Project.java
@@ -1,4 +1,58 @@
 package com.lexshpin.TodoList.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Project")
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    private String name;
+    private String colorCode;
+    private String username;
+
+    public Project() {}
+
+    public Project(String name, String colorCode, String username) {
+        this.name = name;
+        this.colorCode = colorCode;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
