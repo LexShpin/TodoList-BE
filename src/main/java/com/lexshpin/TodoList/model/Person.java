3b7ce1e669e1b241501e7f6638641c0269ffe4fe
lexshpin <lexshpin@gmail.com>
2022-12-11 16:05:44 -0500

    Model for person

--- src/main/java/com/lexshpin/TodoList/model/Person.java
+++ src/main/java/com/lexshpin/TodoList/model/Person.java
@@ -1,4 +1,50 @@
 package com.lexshpin.TodoList.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Person")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+
+    public Person() {}
+
+    public Person(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
